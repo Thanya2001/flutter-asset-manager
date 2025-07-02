48405806dc6d7f122b8def25f217231ea884434e
Thanya <thanyas125@gmail.com>
2025-07-02 03:36:31 -0700

    Fix: base href for GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0f746471adafc59bd51cdd550a43f0ce
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "755bc3f0e491ab994aa39f417a1c8069",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "a3f3a1a02fa0b22b7fb51af5a5332005",
+"flutter_bootstrap.js": "1d576b3450d365a2b00c71452ae4052f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
