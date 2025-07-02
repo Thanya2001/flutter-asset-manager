d77915c5a4bf47f2419d6ef27e6252b6df6d9f6a
Thanya <thanyas125@gmail.com>
2025-07-02 03:58:13 -0700

    Updated UI and logic

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2396808190"
+    serviceWorkerVersion: "3914421548"
   }
 });
