150313c18b5c0962748f139da34b48abf0a0a559
Thanya <thanyas125@gmail.com>
2025-07-03 22:00:53 -0700

    Re-deploy after fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0f746471adafc59bd51cdd550a43f0ce
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "755bc3f0e491ab994aa39f417a1c8069",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "b015763075003209a518ec2e6740f1e5",
+"flutter_bootstrap.js": "f421e5ceed261ad6faa0d53e91714f1f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
